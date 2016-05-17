4a85f89e45f4715b10177cbf65db6b15b5e3ce52
askeing <askeing@gmail.com>
2016-05-17 17:18:11 +0800

    update bug2sum

--- bug2sum.py
+++ bug2sum.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
 import os
-import re
-import json
 import shutil
 import argparse
 import tempfile
 from distutils import dir_util
 from argparse import ArgumentDefaultsHelpFormatter
 
-from time import gmtime, strftime, mktime
-import datetime
-
 
 class Bug2SumGenerator(object):
 
     _SUMMARY_DIR = os.path.join('.', 'summary')
     _TIMEUNIT_EXT = '.timeunit'
+    _TIME_EXT = '.time'
 
     def __init__(self, root_folder):
         if Bug2SumGenerator.check_root_folder(root_folder):
@@ -25,6 +21,15 @@ class Bug2SumGenerator(object):
         else:
             print('Cannot found the Bug_ID and timeunit from {} folder.'.format(root_folder))
             exit(-1)
+
+    @staticmethod
+    def _touch(filename, time=None):
+        if not os.path.exists(filename):
+            try:
+                with open(filename, 'a'):
+                    os.utime(filename, time)
+            except:
+                print('[ERROR] cannot create {} file.'.format(filename))
 
     @staticmethod
     def _get_dir_basename(path):
@@ -36,25 +41,23 @@ class Bug2SumGenerator(object):
             return True
         return False
 
-    def create_bug_folder(self, bug_id):
-        bug_dir = os.path.join(self.tmp_dir, bug_id)
-        if not os.path.exists(bug_dir):
-            os.makedirs(bug_dir)
-        return bug_dir
-
     def generate_summary_content(self, bug_tmp_dir, timeunit):
         timeunit_name = timeunit + Bug2SumGenerator._TIMEUNIT_EXT
         for root, dirs, files in os.walk(bug_tmp_dir):
-            for f in files:
-                os.remove(os.path.join(root, f))
             if not dirs:
                 timeunit_path = os.path.join(root, timeunit_name)
                 if not os.path.exists(timeunit_path):
                     os.makedirs(timeunit_path)
+                for f in files:
+                    time_fname = f.split('.')[0] + Bug2SumGenerator._TIME_EXT
+                    time_path = os.path.join(timeunit_path, time_fname)
+                    Bug2SumGenerator._touch(time_path)
+            for f in files:
+                os.remove(os.path.join(root, f))
 
     def run(self):
         bug_id, time_unit = Bug2SumGenerator._get_dir_basename(self.root_folder).split('.')
-        print('[INFO] Generating BugID: {}, Timeunit" {} ...'.format(bug_id, time_unit))
+        print('[INFO] Generating BugID: {}, Timeunit {} ...'.format(bug_id, time_unit))
         bug_tmp_dir = os.path.join(self.tmp_dir, bug_id)
         shutil.copytree(self.root_folder, bug_tmp_dir)
         self.generate_summary_content(bug_tmp_dir, time_unit)
