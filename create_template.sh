b3474bfa3a2224f19141e78672c42818b6063676
askeing <askeing@gmail.com>
2016-05-18 14:19:54 +0800

    update gitignore and modify the log message of create_template

--- create_template.sh
+++ create_template.sh
@@ -9,7 +9,6 @@ echo "[INFO] Running Generate Template from Summary Folder ..."
 for dir in summary/*/
 do
     DIR=${dir%*/}
-    echo "[INFO] Copying ${DIR##*/} ..."
     cp -r ${DIR}/* ${TMP_DIR}/
 done
 
@@ -19,14 +18,12 @@ echo "[INFO] Running Generate Template from Bugzilla Folder ..."
 for dir in bugzilla/*/
 do
     DIR=${dir%*/}
-    echo "[INFO] Copying ${DIR##*/} ..."
     cp -r ${DIR}/* ${TMP_DIR}/
 done
 
 
 # Remove all files (timeunit and time) in Temp Folder
 pushd ${TMP_DIR}/
-    echo '[INFO] Removing the "timeunit" and "time" folders and files ...'
     find . -type f -name "*.time" -delete
     find . -type f -name "*.time.*" -delete
     find . -type f -regex ".*[0-9]\{1,\}\([.][0-9]\{1,3\}\)\{1,2\}$" -delete
@@ -35,17 +32,16 @@ pushd ${TMP_DIR}/
 popd
 
 # Copy Temp to Template folder
-echo "[INFO] Copying result to template folder ..."
 mkdir -p template
 cp -r ${TMP_DIR}/* template/
 
-echo "[INFO] Creating placeholders ..."
+# Creating placeholders
 find template/ -name "*::*" -print0 | xargs -0 -I % touch %/placeholder
 
 # Remove Temp Folder
 rm -rf "${TMP_DIR}"
 
-#tree template
+# tree template
 echo "[INFO] Generate Template from Summary Done."
 echo '[INFO] Run "$ tree template/" to check the result.'
 
